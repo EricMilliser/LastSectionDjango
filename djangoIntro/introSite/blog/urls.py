ea76dcfb2d02ee0a85cacd52df64d3d90bac838a
EricMilliser <millisereric@gmail.com>
2024-05-04 13:23:51 -0400

    Intro Site

--- djangoIntro/introSite/blog/urls.py
+++ djangoIntro/introSite/blog/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.post_list, name='post_list'),
 ]
--- djangoIntro/introSite/blog/views.py
+++ djangoIntro/introSite/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the Blog index.")
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
